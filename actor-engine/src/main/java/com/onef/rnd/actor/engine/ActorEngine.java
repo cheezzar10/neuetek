8dfd6b0392841724f18b71154dc093474aed0cbd
Andrey Smirnov <andreysmirn@gmail.com>
2021-08-29 21:45:56 +0300

    some experiments with very simple actor model engine implementation

---
+++ actor-engine/src/main/java/com/onef/rnd/actor/engine/ActorEngine.java
@@ -0,0 +1,12 @@
+package com.onef.rnd.actor.engine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ActorEngine {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
+	public void start() {
+		log.debug("actor engine started");
+	}
+}
---
+++ actor-engine/src/test/java/com/onef/rnd/actor/engine/ActorEngineTest.java
@@ -0,0 +1,12 @@
+package com.onef.rnd.actor.engine;
+
+import org.junit.Test;
+
+public class ActorEngineTest {
+	@Test
+	public void testActorEngineStart() {
+		ActorEngine actorEngine = new ActorEngine();
+		
+		actorEngine.start();
+	}
+}
